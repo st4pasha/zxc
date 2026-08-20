5c6ef64793134fd1779d7d1e4a3550904c797bb1
st4pasha <hellobadi0@gmail.com>
2026-08-20 11:28:19 +0300

    Fixed based on the comments.

--- errors/errors.go
+++ errors/errors.go
@@ -1,8 +1,9 @@
-package errors1
+package errors
 
 import "errors"
 
 var (
-	ErrNotFound = errors.New("Ресурс не найден !")
-	ErrMissData = errors.New("Недостаточно данных !")
+	ErrNotFound    = errors.New("resource not found")
+	ErrMissingData = errors.New("insufficient data")
+	ErrParse       = errors.New("parse error")
 )
